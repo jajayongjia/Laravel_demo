ad33e31636f3329f6d279f4541c7ecfe2133c8af
jajayongjia <yongjia@ualberta.ca>
2019-04-12 11:42:11 +0800

    added unit test

--- app/Student.php
+++ app/Student.php
@@ -7,4 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     protected $fillable=['name','phoneNumber','email'];
+    public $name;
+    public $phoneNumber;
+    public $email;
+
+    public function setName($name){
+      $this->name = $name;
+    }
+
+    public function getName(){
+      return $this->name;
+    }
+    public function setPhoneNumber($phoneNumber){
+      $this->phoneNumber = $phoneNumber;
+    }
+
+    public function getPhoneNumber(){
+      return $this->phoneNumber;
+    }
+
+    public function setEmail($email){
+      $this->email = $email;
+    }
+
+    public function getEmail(){
+      return $this->email;
+    }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Student;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -15,5 +16,18 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->assertTrue(true);
+        ;
+    }
+
+    public function testSetterGetter(){
+      $student = new \App\Student;
+      $student->setName('aa');
+      $student->setPhoneNumber('bb');
+      $student->setEmail('cc');
+
+      $this->assertEquals($student->getName(),'aa');
+      $this->assertEquals($student->getPhoneNumber(),'bb');
+      $this->assertEquals($student->getEmail(),'cc');
+
     }
 }
